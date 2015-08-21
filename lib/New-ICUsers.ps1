6d7c66740d99373eedad99a37cceadc1cf2b8669
Pierrick <pierrick.lozach@inin.com>
2015-08-21 01:05:32 -0700

    Adding test to add new icusers in a batch

---
+++ lib/New-ICUsers.ps1
@@ -0,0 +1,33 @@
+﻿<#
+# AUTHOR : Pierrick Lozach
+#>
+
+function New-ICUsers() # {{{2
+{
+# Documentation {{{3
+<#
+.SYNOPSIS
+  Creates new IC users
+.DESCRIPTION
+  Creates new IC users. If passwords are ommitted, default value is '1234'
+.PARAMETER ICSession
+  The Interaction Center Session
+.PARAMETER ICUsers
+  Hashtable of user data, including usernames, passwords and extensions
+#> # }}}3
+  [CmdletBinding()]
+  Param(
+    #[Parameter(Mandatory=$true)] [Alias("Session", "Id")] [ININ.ICSession] $ICSession,
+    [Parameter(Mandatory=$true)] [Alias("Users", "UserData")] [string] $ICUsers
+  )
+
+  <#
+  "{tjejejdjf => {username => testcicuser1, password => 1234, extension => 8002}, wjihsdiuhcsd => {username => testcicuser2, password => 5678, extension => 8003 }}"
+  #>
+
+  $users = ConvertFrom-Json($ICUsers)
+
+  $users
+} # }}}2
+
+#New-ICUsers "{tjejejdjf => {username => testcicuser1, password => 1234, extension => 8002}, wjihsdiuhcsd => {username => testcicuser2, password => 5678, extension => 8003 }}"
