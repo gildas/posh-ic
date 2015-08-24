a060cd34d894a5a0e1c41c20607ed6af7c3710b2
PierrickI3 <pierrick.lozach@inin.com>
2015-08-24 20:27:05 +0200

    Adding New-ICUsers and New-ICWorkgroups to the module

--- lib/Posh-IC.psm1
+++ lib/Posh-IC.psm1
@@ -13,13 +13,15 @@ Update-FormatData -AppendPath .\__display.formats.ps1xml
 . .\Remove-ICSession.ps1
 . .\Get-ICSessionStatus.ps1
 . .\Get-ICUserStatus.ps1
+. .\Get-ICUser.ps1
 . .\Get-ICUsers.ps1
 . .\New-ICUser.ps1
-. .\Get-ICUser.ps1
+. .\New-ICUsers.ps1
 . .\Remove-ICUser.ps1
-. .\Get-ICWorkgroups.ps1
 . .\Get-ICWorkgroup.ps1
+. .\Get-ICWorkgroups.ps1
 . .\New-ICWorkgroup.ps1
+. .\New-ICWorkgroups.ps1
 . .\Remove-ICWorkgroup.ps1
 . .\New-ICConfigurationId.ps1
 . .\Get-IPAProcesses.ps1
@@ -40,13 +42,15 @@ Export-ModuleMember `
     'Remove-ICSession',
     'Get-ICSessionStatus',
     'Get-ICUserStatus',
+    'Get-ICUser',
     'Get-ICUsers',
     'New-ICUser',
-    'Get-ICUser',
+    'New-ICUsers',
     'Remove-ICUser',
-    'Get-ICWorkgroups',
     'Get-ICWorkgroup',
+    'Get-ICWorkgroups',
     'New-ICWorkgroup',
+    'New-ICWorkgroups',
     'Remove-ICWorkgroup',
     'New-ICConfigurationId',
     'Get-IPAProcesses',
