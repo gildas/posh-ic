68e05a60aee5118f47a33d106cc6fb3ce3dca58a
Paul McGurn <paul.mcgurn@gmail.com>
2019-07-24 11:11:06 -0400

    add (slightly) better error handling

--- Remove-ICUser.ps1
+++ Remove-ICUser.ps1
@@ -25,6 +25,7 @@ function Remove-ICUser() # {{{2
   $userExists = Get-ICUser $ICSession -ICUser $ICUser
   if ([string]::IsNullOrEmpty($userExists)) {
     # User does not exist
+    Write-Output "No user found for $ICUser"
     return
   }
 
@@ -33,7 +34,7 @@ function Remove-ICUser() # {{{2
     "ININ-ICWS-CSRF-Token" = $ICSession.token;
   }
 
-  $response = Invoke-RestMethod -Uri "$($ICsession.baseURL)/$($ICSession.id)/configuration/users/$ICUser" -Method Delete -Headers $headers -WebSession $ICSession.webSession -ErrorAction Stop
+  $response = Invoke-RestMethod -Uri "$($ICsession.baseURL)/$($ICSession.id)/configuration/users/$ICUser" -Method Delete -Headers $headers -WebSession $ICSession.webSession #-ErrorAction Stop
   [PSCustomObject] $response
 } # }}}2
 
